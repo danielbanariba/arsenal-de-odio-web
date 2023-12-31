555135bfbbc019196639eeb0bad27e6f08df2411
Daniel Banariba <danielbanariba@protonmail.com>
2023-12-31 07:00:00 -0600

    Animacion de movimientos a la hada de las birrias

--- arsenal_de_odio_web/views/hada.py
+++ arsenal_de_odio_web/views/hada.py
@@ -5,11 +5,26 @@ def hada () -> rx.Component:
         '''
             <head>
                 <style>
+                    @keyframes shakeAndScale {
+                        0% { transform: translate(1px, 1px) rotate(0deg) scale(1); }
+                        10% { transform: translate(-1px, -2px) rotate(-1deg) scale(1.05); }
+                        20% { transform: translate(-3px, 0px) rotate(1deg) scale(1.1); }
+                        30% { transform: translate(3px, 2px) rotate(0deg) scale(1.15); }
+                        40% { transform: translate(1px, -1px) rotate(1deg) scale(1.2); }
+                        50% { transform: translate(-1px, 2px) rotate(-1deg) scale(1.5); }
+                        60% { transform: translate(-3px, 1px) rotate(0deg) scale(1.2); }
+                        70% { transform: translate(3px, 1px) rotate(-1deg) scale(1.15); }
+                        80% { transform: translate(-1px, -1px) rotate(1deg) scale(1.1); }
+                        90% { transform: translate(1px, 2px) rotate(0deg) scale(1.05); }
+                        100% { transform: translate(1px, -2px) rotate(-1deg) scale(1); }
+                    }
+
                     #myImage {
                         position: absolute;
                         width: 5%;
                         height: 5%;
                         transition: top 2s ease-in-out, left 2s ease-in-out; /* Agregar transición */
+                        animation: shakeAndScale 2s infinite; /* Agregar animación de temblor y escala */
                     }
                 </style>
             </head>
